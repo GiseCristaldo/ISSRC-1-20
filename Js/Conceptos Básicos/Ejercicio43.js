ec1775f67509dd786b3f226646cf9fc2ad6e6c46
GiseCristaldo <giselle.cristaldo.1994@gmail.com>
2024-03-18 22:10:29 -0300

    cuarto commit ejercicio del 18 al 20 html

--- Js/Conceptos Básicos/Ejercicio43.js
+++ Js/Conceptos Básicos/Ejercicio43.js
@@ -2,11 +2,11 @@
 //eres estudiante o `false` si no lo eres. Luego, muestra un mensaje
 //que diga "Eres estudiante" o "No eres estudiante" en la consola
 //según el valor de la variable.
-let esEstudiante = false;
+let esEstudiante = true;
 
 if (esEstudiante === false) {
-    console.log('No es estudiante')};
-
+    console.log('No es estudiante');
+}
 if (esEstudiante !== false ) {
-    console.log('Es estudiante');
+   console.log('Es estudiante');
 }
